9a5561b08baad48d43e9562f430ad7658e4f712a
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2025-02-05 19:27:01 +0000

    Add assertions for buffer validity and alignment in DoubleBufferSwapper

--- include/double-buffer-swapper/swapper.hpp
+++ include/double-buffer-swapper/swapper.hpp
@@ -1,24 +1,39 @@
 #pragma once
+#include <cassert> // assert
+#include <cstdint>
 template <typename T>
 class DoubleBufferSwapper
 {
 public:
     constexpr DoubleBufferSwapper(T **active_buffer, T *pre_allocated_buffer)
         : active_buffer_(active_buffer),
-          pre_allocated_buffer_(pre_allocated_buffer) {}
+          pre_allocated_buffer_(pre_allocated_buffer)
+    {
+        assert(active_buffer_ != nullptr && "active_buffer_ is nullptr");
+        assert(pre_allocated_buffer_ != nullptr && "pre_allocated_buffer_ is nullptr");
+    }
 
     constexpr inline void set_active(T *buffer) noexcept
     {
+        assert(buffer != nullptr && "buffer is nullptr");
+        assert(buffer != *active_buffer_ && "buffer == *active_buffer_");
         *active_buffer_ = buffer;
     }
 
     constexpr inline void stage(T *buffer) noexcept
     {
+        assert(buffer != nullptr && "buffer is nullptr");
         stage_ = buffer;
     }
 
     constexpr inline void swap() noexcept
     {
+        assert(pre_allocated_buffer_ && "pre_allocated_buffer_ is null!");
+        assert(stage_ && "stage_ is null!");
+        assert(pre_allocated_buffer_ != stage_ && "pre_allocated_buffer_ == stage_");
+        assert(reinterpret_cast<std::uintptr_t>(pre_allocated_buffer_) % alignof(T) == 0 && "unaligned pre_allocated_buffer_");
+        assert(reinterpret_cast<std::uintptr_t>(stage_) % alignof(T) == 0 && "unaligned stage_");
+
         *pre_allocated_buffer_ = *stage_;
         *active_buffer_ = pre_allocated_buffer_;
         stage_ = nullptr;
--- test/double_buffer_swapper_test.cpp
+++ test/double_buffer_swapper_test.cpp
@@ -1,32 +1,77 @@
 #include "double-buffer-swapper/swapper.hpp"
-#include <cassert>
 #include <fmt/core.h>
-void test_preallocated_double_buffer()
+#include <memory> // std::unique_ptr
+
+// define LargeStruct globally to make it accessible
+struct LargeStruct
+{
+    static constexpr std::size_t SIZE_24_MB = 24 * 1024 * 1024;
+    std::size_t id;
+    char data[SIZE_24_MB];
+};
+
+// specialize the fmt formatter for LargeStruct
+template <>
+struct fmt::formatter<LargeStruct>
+{
+    constexpr auto parse(fmt::format_parse_context &ctx) { return ctx.begin(); }
+
+    template <typename FormatContext>
+    auto format(const LargeStruct &, FormatContext &ctx)
+    {
+        return fmt::format_to(ctx.out(), "[LargeStruct: {} bytes]", sizeof(LargeStruct));
+    }
+};
+
+void test_preallocated_double_buffer_int()
 {
     int *active_buffer = nullptr;
     int pre_allocated_buffer = 1;
     DoubleBufferSwapper<int> buffer(&active_buffer, &pre_allocated_buffer);
     int b = 3;
-    fmt::print(
-        "Initial active_buffer addr: {}.\nPre allocated buffer adddr {} and "
-        "value {}\n",
-        static_cast<void *>(active_buffer),
-        static_cast<void *>(&pre_allocated_buffer), pre_allocated_buffer);
+    fmt::print("Initial active_buffer addr: {}.\nPre-allocated buffer addr: {} and value: {}\n",
+               static_cast<void *>(active_buffer),
+               static_cast<void *>(&pre_allocated_buffer), pre_allocated_buffer);
 
     buffer.set_active(&b);
     assert(active_buffer == &b);
-    fmt::print("Set active_buffer addr: {} value: {}\n",
-               static_cast<void *>(active_buffer), *active_buffer);
+    fmt::print("Set active_buffer addr: {} value: {}\n", static_cast<void *>(active_buffer), *active_buffer);
+
     buffer.stage(&b);
     buffer.swap();
     assert(active_buffer == &pre_allocated_buffer);
-    fmt::print("Swapped active bufferes: {} value: {}\n",
-               static_cast<void *>(active_buffer), *active_buffer);
+    fmt::print("Swapped active buffers: {} value: {}\n", static_cast<void *>(active_buffer), *active_buffer);
+}
+
+void test_preallocated_large_struct()
+{
+    LargeStruct *active_buffer = nullptr;
+    std::unique_ptr<LargeStruct> pre_allocated_buffer = std::make_unique<LargeStruct>();
+    pre_allocated_buffer->id = 1;
+    DoubleBufferSwapper<LargeStruct> buffer(&active_buffer, pre_allocated_buffer.get());
+    std::unique_ptr<LargeStruct> b = std::make_unique<LargeStruct>();
+    b->id = 2;
+
+    fmt::print("Initial active_buffer addr: {}.\nPre-allocated buffer addr: {} and id: {}\n",
+               static_cast<void *>(active_buffer),
+               static_cast<void *>(pre_allocated_buffer.get()), pre_allocated_buffer->id);
+
+    buffer.set_active(b.get());
+    assert(active_buffer == b.get());
+    assert(active_buffer->id == 2);
+    fmt::print("Set active_buffer addr: {} id: {}\n", static_cast<void *>(active_buffer), active_buffer->id);
+
+    buffer.stage(b.get());
+    buffer.swap();
+    assert(active_buffer == pre_allocated_buffer.get());
+    assert(active_buffer->id == 2);
+    fmt::print("Swapped active buffers: {} id: {}\n", static_cast<void *>(active_buffer), active_buffer->id);
 }
 
 int main()
 {
-    test_preallocated_double_buffer();
+    test_preallocated_double_buffer_int();
+    test_preallocated_large_struct();
     fmt::print("Test passed.\n");
     return 0;
 }
